056bccf39548c5e61d8f4b93c8f0d099095f68b9
Mike Treaster <treaster@gmail.com>
2021-06-23 11:38:19 -0700

    add mouseenter, mouseleave. modernize JS
    Implement onmouseenter and onmouseleave event handling.
    Modern JS syntax - var vs let, == vs ===, comparison to undefined

--- mithril_util.js
+++ mithril_util.js
@@ -1,11 +1,11 @@
-var MITHRIL_UTIL = {};
+let MITHRIL_UTIL = {};
 
 
 // Routing help
 
 MITHRIL_UTIL._routerParams = {};
 muRoute = function(url, params) {
-  if (params == undefined) {
+  if (!params) {
     params = {};
   }
   MITHRIL_UTIL._routerParams = params;
@@ -13,12 +13,12 @@ muRoute = function(url, params) {
 }
 
 muGetAndClearParams = function() {
-  var params = MITHRIL_UTIL._routerParams;
+  let params = MITHRIL_UTIL._routerParams;
   MITHRIL_UTIL._routerParams = {};
   return params;
 }
 
-var softRoute = function(path) {
+let softRoute = function(path) {
   if (window.location.pathname !== path) {
     window.history.pushState({}, "", path);
   }
@@ -38,6 +38,8 @@ MITHRIL_UTIL.Element = function(tagName) {
   this._tm_configs = [];
   this._tm_mouseovers = [];
   this._tm_mouseouts = [];
+  this._tm_mouseenters = [];
+  this._tm_mouseleaves = [];
   this._tm_mouseups = [];
 };
 
@@ -45,13 +47,13 @@ MITHRIL_UTIL.Element.prototype = new Object();
 MITHRIL_UTIL.Element.prototype.constructor = MITHRIL_UTIL.Element;
 
 MITHRIL_UTIL.Element.prototype._initChildren = function() {
-  if (this.children === undefined) {
+  if (!this.children) {
     this.children = [];
   }
 }
 
 MITHRIL_UTIL.Element.prototype.num_children = function() {
-  if (this.children === undefined) {
+  if (!this.children) {
     return 0;
   }
   return this.children.length;
@@ -84,9 +86,9 @@ MITHRIL_UTIL.Element.prototype.click = function(onClick) {
 
 MITHRIL_UTIL.Element.prototype.config = function(config) {
   this._tm_configs.push(config);
-  if (this.attrs['config'] === null) {
+  if (!this.attrs['config']) {
     this.attrs['config'] = function(element, isInitialized, context) {
-      for (var i = 0; i < this._tm_configs.length; ++i) {
+      for (let i = 0; i < this._tm_configs.length; ++i) {
         this._tm_configs[i](element, isInitialized, context);
       }
     }.bind(this);
@@ -146,9 +148,9 @@ MITHRIL_UTIL.Element.prototype.mousemove = function(onMouseMove) {
 
 MITHRIL_UTIL.Element.prototype.mouseover = function(onMouseOver) {
   this._tm_mouseovers.push(onMouseOver);
-  if (this.attrs['onmouseover'] === null) {
+  if (!this.attrs['onmouseover']) {
     this.attrs['onmouseover'] = function(evt) {
-      for (var i = 0; i < this._tm_mouseovers.length; ++i) {
+      for (let i = 0; i < this._tm_mouseovers.length; ++i) {
         this._tm_mouseovers[i](evt);
       }
     }.bind(this);
@@ -158,10 +160,34 @@ MITHRIL_UTIL.Element.prototype.mouseover = function(onMouseOver) {
 
 MITHRIL_UTIL.Element.prototype.mouseout = function(onMouseOut) {
   this._tm_mouseouts.push(onMouseOut);
-  if (this.attrs['onmouseout'] === null) {
+  if (!this.attrs['onmouseout']) {
     this.attrs['onmouseout'] = function(evt) {
-      for (var i = 0; i < this._tm_mouseouts.length; ++i) {
+      for (let i = 0; i < this._tm_mouseouts.length; ++i) {
         this._tm_mouseouts[i](evt);
+      }
+    }.bind(this);
+  }
+  return this;
+};
+
+MITHRIL_UTIL.Element.prototype.mouseenter = function(onMouseEnter) {
+  this._tm_mouseenters.push(onMouseEnter);
+  if (!this.attrs['onmouseenter']) {
+    this.attrs['onmouseenter'] = function(evt) {
+      for (let i = 0; i < this._tm_mouseenters.length; ++i) {
+        this._tm_mouseenters[i](evt);
+      }
+    }.bind(this);
+  }
+  return this;
+};
+
+MITHRIL_UTIL.Element.prototype.mouseleave = function(onMouseLeave) {
+  this._tm_mouseleaves.push(onMouseLeave);
+  if (!this.attrs['onmouseleave']) {
+    this.attrs['onmouseleave'] = function(evt) {
+      for (let i = 0; i < this._tm_mouseleaves.length; ++i) {
+        this._tm_mouseleaves[i](evt);
       }
     }.bind(this);
   }
@@ -170,9 +196,9 @@ MITHRIL_UTIL.Element.prototype.mouseout = function(onMouseOut) {
 
 MITHRIL_UTIL.Element.prototype.mouseup = function(onMouseuUp) {
   this._tm_mouseups.push(onMouseuUp);
-  if (this.attrs['onmouseup'] === null) {
+  if (!this.attrs['onmouseup']) {
     this.attrs['onmouseup'] = function(evt) {
-      for (var i = 0; i < this._tm_mouseups.length; ++i) {
+      for (let i = 0; i < this._tm_mouseups.length; ++i) {
         this._tm_mouseups[i](evt);
       }
     }.bind(this);
@@ -233,8 +259,8 @@ MITHRIL_UTIL.Element.prototype.key = function(key) {
 };
 
 MITHRIL_UTIL.Element.prototype.addClass = function(className) {
-  var origClass = this.attrs['class'];
-  if (origClass == undefined) {
+  let origClass = this.attrs['class'];
+  if (!origClass) {
     this.attrs['class'] = className;
   } else {
     this.attrs.class = origClass + ' ' + className;
@@ -242,12 +268,36 @@ MITHRIL_UTIL.Element.prototype.addClass = function(className) {
   return this;
 };
 
+MITHRIL_UTIL.Element.prototype.addClassIf = function(condition, className) {
+  if (!condition) {
+      return this;
+  }
+  let origClass = this.attrs['class'];
+  if (!origClass) {
+    this.attrs['class'] = className;
+  } else {
+    this.attrs.class = origClass + ' ' + className;
+  }
+  return this;
+};
+
+// DEPRECATED. This function makes no sense in a mithril-style engine.
 MITHRIL_UTIL.Element.prototype.removeClass = function(className) {
-  var oldValue = this.attrs.class;
+  let oldValue = this.attrs.class;
   if (oldValue !== undefined) {
-    var re = new RegExp('\\b' + className + '\\b', 'g');
+    let re = new RegExp('\\b' + className + '\\b', 'g');
     this.attrs.class = oldValue.replace(re, '');
   }
+};
+
+MITHRIL_UTIL.Element.prototype.style = function(attribute, value) {
+  let style = {};
+  if ('style' in this.attrs) {
+      style = this.attrs.style;
+  }
+  style[attribute] = value;
+  this.attrs.style = style;
+  return this;
 };
 
 MITHRIL_UTIL.Element.prototype.appendTo = function(parentElement) {
@@ -256,13 +306,17 @@ MITHRIL_UTIL.Element.prototype.appendTo = function(parentElement) {
 };
 
 MITHRIL_UTIL.Element.prototype.append = function(child) {
+  if (this.text) {
+      console.log(`mu warning: element "${this.tag}" cannot have both text and child elements`);
+  }
+
   this._initChildren();
   this.children.push(child);
   return this;
 };
 
 MITHRIL_UTIL.Element.prototype.appendList = function(children) {
-    for (var i = 0; i < children.length; ++i) {
+    for (let i = 0; i < children.length; ++i) {
         this.append(children[i]);
     }
     return this;
@@ -277,14 +331,17 @@ MITHRIL_UTIL.Element.prototype.appendIf = function(condition, child) {
 };
 
 MITHRIL_UTIL.Element.prototype.map = function(elements, func) {
-    for (var i = 0; i < elements.length; ++i) {
-        var item = func(elements[i]);
+    for (let i = 0; i < elements.length; ++i) {
+        let item = func(elements[i]);
         this.append(item);
     }
     return this;
 }
 
 MITHRIL_UTIL.Element.prototype.setText = function(text) {
+  if (this.children.length > 0) {
+      console.log(`mu warning: element "${this.tag}" cannot have both text and child elements`);
+  }
   if (typeof text === 'number') {
     text = '' + text;
   }
@@ -292,11 +349,11 @@ MITHRIL_UTIL.Element.prototype.setText = function(text) {
     if (text !== '') {
       this.text = text;
     } else {
-      console.log('Using empty string in mithril_util text().');
+      console.log('mu warning: Using empty string in mithril_util text().');
     }
   } else {
     this.text = 'mithril_util error';
-    console.log('Non-string, non-number argument to text(): "' + text + '"');
+    console.log('mu warning: Non-string, non-number argument to text(): "' + text + '"');
   }
   return this;
 };
